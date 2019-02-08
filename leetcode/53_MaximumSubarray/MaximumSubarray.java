1d7baf7dd44d888a387974aa0c8b14cee60cbe68
YangHu-yh <huyangyoung1996@gmail.com>
2019-02-08 00:18:08 -0600

    update

--- leetcode/53_MaximumSubarray/MaximumSubarray.java
+++ leetcode/53_MaximumSubarray/MaximumSubarray.java
@@ -13,19 +13,22 @@
 // Dynamic Programming Solution
 class MaximumSubarray {
     public static int maxSubArray(int[] A) {
+        System.out.println( A );
         int n = A.length;
         int max_endinghere = 0;
         max_endinghere = A[0];
         int max = max_endinghere;
+        System.out.println("Start with: n = "+ n +"; max = max_endinghere = "+ max_endinghere);
         for(int i = 1; i < n; i++){
             max_endinghere = Math.max(max_endinghere + A[i], A[i]);
             max = Math.max(max, max_endinghere);
+            System.out.println("i = "+ i +"; max_endinghere = " + max_endinghere + "; max = " + max);
         }
         return max;
     }
 
     public static void main(String[] args){
-        int[] MainArray = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        int[] MainArray = new int[]{-2,1,-3,4,-1,-1,2,1,-5,4};
         System.out.println(maxSubArray(MainArray));
     }
 }
